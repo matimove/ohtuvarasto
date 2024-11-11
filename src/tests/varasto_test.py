69adcbbba9cb24495d2857eb249711aa772711c2
Timo Mäkelä <timo.v.makela@helsinki.fi>
2024-11-11 18:43:04 +0200

    rikotaan pylint sääntö

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -6,7 +6,7 @@ class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
 
-    def test_konstruktori_luo_tyhjan_varaston(self):
+    def test_konstruktori_luo_tyhjan_varaston(self, toinen_argumentti):
         """Testaa konstruktoria"""
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
