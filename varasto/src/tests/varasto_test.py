d3e3a7325ca9829bfd7c4ae7d8c4c2079a75b8d8
Timo Mäkelä <timo.v.makela@helsinki.fi>
2024-11-11 18:26:41 +0200

    Viikko 2 Tehtävä 3 tehty

---
+++ varasto/src/tests/varasto_test.py
@@ -0,0 +1,77 @@
+"""Auttaa unittestien tekemisessä"""
+import unittest
+from varasto import Varasto
+class TestVarasto(unittest.TestCase):
+    """Toteuttaa testit varastoluokalle"""
+    def setUp(self):
+        self.varasto = Varasto(10)
+
+    def test_konstruktori_luo_tyhjan_varaston(self):
+        """Testaa konstruktoria"""
+        # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_uudella_varastolla_oikea_tilavuus(self):
+        """Testaa tilavuuden toiminnan"""
+        self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+    def test_lisays_lisaa_saldoa(self):
+        """Testaa lisäyksen"""
+        self.varasto.lisaa_varastoon(8)
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
+        """Testaa lisäystä"""
+        self.varasto.lisaa_varastoon(8)
+        # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_ottaminen_palauttaa_oikean_maaran(self):
+        """Testaa ottamista ja määrää"""
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(2)
+        self.assertAlmostEqual(saatu_maara, 2)
+
+    def test_ottaminen_lisaa_tilaa(self):
+        """Testaa ottamista"""
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.ota_varastosta(2)
+        # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_tilavuus_nollataan(self):
+        """Testaa virheellistä tilavuutta"""
+        self.varasto = Varasto(-2,-2)
+        self.assertEqual(self.varasto.paljonko_mahtuu(), 0.0)
+
+    def test_konstruktori_hukkaa_ylimääräisen_saldon(self):
+        """Testaa ylimääräistä saldoa"""
+        self.varasto = Varasto(2,4)
+        self.assertEqual(self.varasto.paljonko_mahtuu(), 0.0)
+
+    def test_negatiivinen_lisäys_ei_vaikuta(self):
+        """Testaa negatiivista lisäystä"""
+        self.varasto.lisaa_varastoon(-8)
+        self.assertEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_ylimääräinen_saldo_lisäys_hukkaan(self):
+        """Testaa ylimääräistä saldoa"""
+        self.varasto.lisaa_varastoon(12)
+        self.assertEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_otetaan_negatiivinen_luku_saldosta(self):
+        """Testaa negatiivisen luvun ottamista"""
+        self.varasto.lisaa_varastoon(6)
+        self.varasto.ota_varastosta(-2)
+        self.assertEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_otetaan_kaikki_mitä_voidaan_saldosta(self):
+        """Testataan kaiken ottamista"""
+        self.varasto.lisaa_varastoon(6)
+        self.varasto.ota_varastosta(7)
+        self.assertEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_tulostus_näkyy_oikein(self):
+        """Testataan tulostusta"""
+        self.varasto.lisaa_varastoon(6)
+        self.assertEqual(str(self.varasto), "saldo = 6, vielä tilaa 4")
---
+++ varasto/src/varasto.py
@@ -0,0 +1,50 @@
+"""Tämä moduuli vastaa varastoluokan toteutuksesta"""
+class Varasto: #muutos
+    """Varasto luokka toteuttaa varaston ja siihen liittyvät metodit"""
+    def __init__(self, tilavuus, alku_saldo = 0):
+        if tilavuus > 0.0:
+            self.tilavuus = tilavuus
+        else:
+            # virheellinen, nollataan
+            self.tilavuus = 0.0
+
+        if alku_saldo < 0.0:
+            # virheellinen, nollataan
+            self.saldo = 0.0
+        elif alku_saldo <= tilavuus:
+            # mahtuu
+            self.saldo = alku_saldo
+        else:
+            # täyteen ja ylimäärä hukkaan!
+            self.saldo = tilavuus
+
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    def paljonko_mahtuu(self):
+        """Kertoo paljonko varastoon mahtuu"""
+        return self.tilavuus - self.saldo
+
+    def lisaa_varastoon(self, maara):
+        """Lisää varastoon tuotteita"""
+        if maara < 0:
+            return
+        if maara <= self.paljonko_mahtuu():
+            self.saldo = self.saldo + maara
+        else:
+            self.saldo = self.tilavuus
+
+    def ota_varastosta(self, maara):
+        """Ota varastosta tuotteita"""
+        if maara < 0:
+            return 0.0
+        if maara > self.saldo:
+            kaikki_mita_voidaan = self.saldo
+            self.saldo = 0.0
+
+            return kaikki_mita_voidaan
+
+        self.saldo = self.saldo - maara
+
+        return maara
+
+    def __str__(self):
+        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
